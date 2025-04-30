9163cf52292d0d6a48f8643e91149fe26cacbbfd
RupanR <rupanmentor@gmail.com>
2025-04-30 20:40:49 +0530

    updated

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-    return (
-        <div>
-            
+  const [formData, setFormData] = useState({
+    productName: "",
+    productPrice: "",
+    productDescription: "",
+    productImage: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        "https://68121ef53ac96f7119a6e923.mockapi.io/api/products",
+        formData
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    const { name, value } = e.target;
+    setFormData((preData) => ({
+      ...preData,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <>
+      <form className="max-w-sm mx-auto mt-5" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label
+           
+            className="block mb-2 text-sm font-medium text-black-900"
+          >
+            Product Name
+          </label>
+          <input
+            type="text"
+            value={formData.productName}
+            onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-black"
+            placeholder="Enter Your Product Name"
+            required
+          />
         </div>
-    );
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Create;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://68121ef53ac96f7119a6e923.mockapi.io/api/products"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex flex-1 flex-wrap gap-6 mt-5">
+      {data.map((ele) => {
+        return (
+          <div key={ele.productId}>
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+              <img className="rounded-t-lg" src={ele.productImage} alt="image" />
+
+              <div className="p-5">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {ele.productName}
+                </h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {ele.productPrice}
+                </h5>
+
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {ele.productDescription}
+                </p>
+                <button
+                  type="button"
+                  className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
